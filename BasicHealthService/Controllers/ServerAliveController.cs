66e7d02615c1e3f4763ebc685dcafdfc8914854f
Pavlo Tunyk <pasha.tunik@gmail.com>
2022-10-23 14:10:55 +0200

    Basic HealthStatus server is ready and send logs to Elastic Search.

---
+++ BasicHealthService/Controllers/ServerAliveController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace BasicHealthService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServerAliveController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public ServerAliveController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new OkResult();
+        }
+    }
+}
